482d50cca21f7c9f680685e9ee9d0221f35bd5b7
eloynaveira <eloy_guisamo@hotmail.com>
2021-04-24 09:52:46 +0200

    Initial migration - Persistence

--- TiendaServicios/TiendaServicios.Api.Autor/Startup.cs
+++ TiendaServicios/TiendaServicios.Api.Autor/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Persistencia;
 
 namespace TiendaServicios.Api.Autor
 {
@@ -25,6 +27,11 @@ namespace TiendaServicios.Api.Autor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<AutorContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("ConexionDatabase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
