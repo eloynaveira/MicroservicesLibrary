4df0f821a764a37e2db3b3433f37666707f8fdd3
eloynaveira <eloy_guisamo@hotmail.com>
2021-04-28 21:10:56 +0200

    First migration model

--- TiendaServicios/TiendaServicios.Api.CarritoCompras/Startup.cs
+++ TiendaServicios/TiendaServicios.Api.CarritoCompras/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
 
 namespace TiendaServicios.Api.CarritoCompras
 {
@@ -25,6 +27,11 @@ namespace TiendaServicios.Api.CarritoCompras
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<CarritoContexto>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("ConexionDatabase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
