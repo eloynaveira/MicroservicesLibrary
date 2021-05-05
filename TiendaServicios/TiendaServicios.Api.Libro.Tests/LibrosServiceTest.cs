b39ba0f99218b45fe80556dec66f225ace8a0f0c
eloynaveira <eloy_guisamo@hotmail.com>
2021-05-05 13:56:32 +0200

    Async classes to tests

--- TiendaServicios/TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios/TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using AutoMapper;
+using GenFu;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using TiendaServicios.Api.Libro.Modelo;
+using TiendaServicios.Api.Libro.Persistencia;
+using TiendaServicios.Api.Libro.Servicios;
+using TiendaServicios.Api.Libro.Servicios.DTOs;
+using Xunit;
 
 namespace TiendaServicios.Api.Libro.Tests
 {
-    class LibrosServiceTest
+    public class LibrosServiceTest
     {
+        private IEnumerable<LibreriaMaterial> ObtenerDatosPrueba()
+        {
+            A.Configure<LibreriaMaterial>()
+                .Fill(x => x.Titulo).AsArticleTitle()
+                .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
+
+            var lista = A.ListOf<LibreriaMaterial>(30);
+            lista[0].LibreriaMaterialId = Guid.Empty;
+
+            return lista;
+        }
+
+        private Mock<ContextoLibreria> CrearContexto()
+        {
+            var datosPrueba = ObtenerDatosPrueba().AsQueryable();
+
+            var dbSet = new Mock<DbSet<LibreriaMaterial>>();
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Provider).Returns(datosPrueba.Provider);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression).Returns(datosPrueba.Expression);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType).Returns(datosPrueba.ElementType);
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator()).Returns(datosPrueba.GetEnumerator());
+        }
+
+        [Fact]
+        public void GetLibros()
+        {
+            var mockContexto = new Mock<ContextoLibreria>();
+            var mockMapper = new Mock<IMapper>();
+            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mockMapper.Object);
+
+        }
     }
 }
